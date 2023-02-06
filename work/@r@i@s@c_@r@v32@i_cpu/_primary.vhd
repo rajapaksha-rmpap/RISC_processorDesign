aae8762f7a1a34c87cf3eaca2c0b9c6ec617209b
rajapaksha-rmpap <rajapaksharmpap@gmail.com>
2023-02-06 20:40:08 +0530

    created L%S paths and control signals in cpu

--- work/@r@i@s@c_@r@v32@i_cpu/_primary.vhd
+++ work/@r@i@s@c_@r@v32@i_cpu/_primary.vhd
@@ -3,11 +3,12 @@ use verilog.vl_types.all;
 entity RISC_RV32I_cpu is
     port(
         instr           : in     vl_logic_vector(31 downto 0);
-        data            : inout  vl_logic_vector(31 downto 0);
         instrAddr       : out    vl_logic_vector(31 downto 0);
+        toMem           : inout  vl_logic_vector(31 downto 0);
+        fromMem         : inout  vl_logic_vector(31 downto 0);
         MemAddr         : out    vl_logic_vector(31 downto 0);
-        MemWrite        : out    vl_logic;
-        MemRead         : out    vl_logic;
+        EnWrite         : out    vl_logic;
+        EnRead          : out    vl_logic;
         addMemControl   : out    vl_logic_vector(1 downto 0);
         clk             : in     vl_logic
     );
